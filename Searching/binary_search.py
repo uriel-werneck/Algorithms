0c6f1390291e11c7f1c8cb401a86350649866b90
Uriel Werneck de Alcântara <131367427+uriel-werneck@users.noreply.github.com>
2024-10-03 02:16:06 -0300

    Update binary_search.py

--- Searching/binary_search.py
+++ Searching/binary_search.py
@@ -1,14 +1,14 @@
-def binary_search(array: list, value: int) -> bool:
+def binary_search(array: list, value: int) -> int:
     left = 0
     right = len(array) - 1
     
     while left <= right:
         mid = (left + right) // 2
         if array[mid] == value:
-            return True
+            return mid
         elif array[mid] > value:
             right = mid - 1
         elif array[mid] < value:
             left = mid + 1
     
-    return False
+    return -1
