992252934131ec6105817f943eff7ef8951c996d
Uriel Werneck de Alcântara <131367427+uriel-werneck@users.noreply.github.com>
2024-10-10 23:15:14 -0300

    Update jump_search.py

--- Searching/jump_search.py
+++ Searching/jump_search.py
@@ -2,7 +2,7 @@ def jump_search(array: list, value: int) -> int:
     """ Time Complexity: O(√n) """
     step = round(len(array)**(1/2))
     current = 0
-
+    
     while array[current] < value:
         if current + step > len(array) - 1:
             current = len(array) - 1
@@ -14,5 +14,3 @@ def jump_search(array: list, value: int) -> int:
         if array[i] == value:
             return i
     return -1
-
-print(jump_search([2, 5, 10, 14, 20, 21], 14)) # returns 3
